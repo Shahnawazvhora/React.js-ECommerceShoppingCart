e5ecaab234f398b2a8fca57c436d036201afae66
Shahnawaz Vhora <shahnawazvhora7333@gmail.com>
2023-05-12 16:20:58 +0530

    Second Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Products from './Products';
-import { faCircleArrowUp  } from '@fortawesome/free-solid-svg-icons';
+import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import loaderDots from "../images/loaderdots.gif";
+import errorImg from "../images/errorImg.jpg";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -17,8 +19,9 @@ const Home = () => {
     }).then((response) => {
       setPosts(response.data);
       setLoading(false);
-    }).catch((error) => {
-      console.log(error);
+    }).catch(() => {
+      setError(true);
+      setLoading(false);
     })
   }, []);
 
@@ -28,27 +31,35 @@ const Home = () => {
         {
           loading ? (
             <div className="loader-container d-flex align-items-center justify-content-center">
-            <div className="text-info">
-              <img src={loaderDots} alt="loader" />
+              <div className="text-info">
+                <img src={loaderDots} alt="loader" />
+              </div>
             </div>
-          </div>
+          ) : error ? (
+            <div className='errorhandle'>
+              <img src={errorImg} alt="error" className='error' />
+              <div>
+                <button className='btn btn-tryagain me-4'>Try again</button>
+                <button className='btn btn-tryagain'>Report an issue</button>
+              </div>
+            </div>
           ) : (
-        <div className="row mt-4">
-          {
-            posts.map((item, index) => {
-              return (
-                <div key={index} className="col-md-4">
-                  <div className="card-deck mt-4">
-                    <Products key={item.id} item={item} />
-                  </div>
-                </div>
-              )
-            })
-          }
-        <button className="btn btn-primary scroll-top" onClick={() => {window.scrollTo(0, 0);}}>
-          <FontAwesomeIcon icon={faCircleArrowUp} />
-        </button>
-        </div>
+            <div className="row mt-4">
+              {
+                posts.map((item, index) => {
+                  return (
+                    <div key={index} className="col-md-4">
+                      <div className="card-deck mt-4">
+                        <Products key={item.id} item={item} />
+                      </div>
+                    </div>
+                  )
+                })
+              }
+              <button className="btn btn-primary scroll-top" onClick={() => { window.scrollTo(0, 0); }}>
+                <FontAwesomeIcon icon={faCircleArrowUp} />
+              </button>
+            </div>
           )
         }
       </div>
